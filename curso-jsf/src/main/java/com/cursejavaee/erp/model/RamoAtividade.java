678b69b2a9f21dee2ca8df64794efcbdfadfa868
Wellington Delmondes <wellington.apk@gmail.com>
2023-03-07 23:08:53 -0300

    Iniciando Estudos JAVA EE.

---
+++ curso-jsf/src/main/java/com/cursejavaee/erp/model/RamoAtividade.java
@@ -0,0 +1,10 @@
+package com.cursejavaee.erp.model;
+
+import java.io.Serializable;
+
+public class RamoAtividade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
