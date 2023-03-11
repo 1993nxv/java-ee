8742c79b7dd40932d411128f7e10817f03b9f13b
Wellington Delmondes <wellington.apk@gmail.com>
2023-03-11 19:02:46 -0300

    Camada de Persistencia - Erro resolvido.

--- curso-jsf/src/main/java/com/cursejavaee/erp/model/Empresa.java
+++ curso-jsf/src/main/java/com/cursejavaee/erp/model/Empresa.java
@@ -2,6 +2,7 @@ package com.cursejavaee.erp.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,6 +16,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+
 
 @Entity
 @Table(name = "empresa")
@@ -78,6 +81,14 @@ public class Empresa implements Serializable {
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+	
+	public TipoEmpresa getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEmpresa tipo) {
+		this.tipo = tipo;
+	}
 
 	public Date getDataFundacao() {
 		return dataFundacao;
@@ -94,21 +105,10 @@ public class Empresa implements Serializable {
 	public void setRamoAtividade(RamoAtividade ramoAtividade) {
 		this.ramoAtividade = ramoAtividade;
 	}
-	
-	public TipoEmpresa getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoEmpresa tipo) {
-		this.tipo = tipo;
-	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -120,16 +120,12 @@ public class Empresa implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Empresa other = (Empresa) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(id, other.id);
 	}
 
 	@Override
 	public String toString() {
-		return "Empresa [id=" + id + "]";
+		return "Empresa id: " + id;
 	}
+	
 }
