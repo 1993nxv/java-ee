74014488ef00f413c2137cdfc5dc5fe4142496fe
Wellington Delmondes <wellington.apk@gmail.com>
2023-03-15 20:24:08 -0300

    Listando empresas com tabela de dados.

--- curso-jsf/src/main/java/com/cursejavaee/erp/controller/GestaoEmpresasBean.java
+++ curso-jsf/src/main/java/com/cursejavaee/erp/controller/GestaoEmpresasBean.java
@@ -1,12 +1,15 @@
 package com.cursejavaee.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.cursejavaee.erp.model.Empresa;
-import com.cursejavaee.erp.model.TipoEmpresa;
+import com.cursejavaee.erp.repository.Empresas;
+
 
 @Named
 @ViewScoped
@@ -14,25 +17,17 @@ public class GestaoEmpresasBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Empresa empresa = new Empresa();
+	@Inject
+	private Empresas empresas;
 	
-	public void salvar() {
-		System.out.println(
-				"Razão social: " + empresa.getRazaoSocial()
-				+ " - Nome fantasia: " + empresa.getNomeFantasia()
-				+ " - Tipo: " + empresa.getTipo());
+	private List<Empresa> listaEmpresas;
+	
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
 	}
 	
-	public String ajuda() {
-		return "Ajuda?faces-redirect=true";
-	}
-	
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-	
-	public TipoEmpresa[] getTiposEmpresa() {
-		return TipoEmpresa.values();
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
 	}
 
 }
